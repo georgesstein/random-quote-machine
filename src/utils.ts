c05fce528c247a403abaf97909cb9669e3162aaa
Georges Markosian <georgesmarkosian@gmail.coma>
2022-09-16 19:25:42 +0400

    Create utility function for random color

---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+const getRandom = (max: number) => {
+  return Math.floor(Math.random() * max)
+}
+
+export const chooseRandomColor = (colors: string[], exceptionColor: string) => {
+  const idx = getRandom(colors.length)
+  const nextColor = colors[idx]
+
+  if (nextColor === exceptionColor) {
+    return colors[idx === colors.length - 1 ? 0 : idx + 1]
+  }
+
+  return nextColor
+}
